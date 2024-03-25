a0329b168c417409d3e80bfbae17a900a04df7bf
James Boyd <jamesboyd2011@live.com>
2024-03-25 19:15:07 -0400

    Added routes

--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const {
   client,
   createTables,
@@ -9,6 +12,54 @@ const {
   fetchReservations,
   destroyReservation
 } = require("./db");
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/customers", async (req, res, next) => {
+  try {
+    res.send(await fetchCustomers());
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.get("/api/restaurants", async (req, res, next) => {
+  try {
+    res.send(await fetchRestaurants());
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+
+app.get("/api/reservations", async (req, res, next) => {
+  try {
+    res.send(await fetchReservations());
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+
+
+app.post("/api/customers/:id/reservations", async (req, res, next) => {
+  try {
+    res.status(201).send(await createReservation(req.params));
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.delete("/api/customers/:id/reservations/:id", async (req, res, next) => {
+  try {
+    res.status(204).send(await destroyReservation(req.params));
+  } catch (ex) {
+    next(ex);
+  }
+});
 
 const init = async () => {
   console.log("conneting to database");
@@ -34,8 +85,11 @@ const init = async () => {
   ]);
   console.log(await fetchReservations())
   
-  destroyReservation({id:vacation.id,customer_id:james.id});
+  // destroyReservation({id:vacation.id,customer_id:james.id});
   console.log(await fetchReservations());
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listening on port ${port}`));
 };
 
 init();
